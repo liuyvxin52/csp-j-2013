52ca39b4f9bea01988595fb70753aedbb1c119d3
liuyvxin52 <xinxintpai@outlook.com>
2023-08-15 21:00:54 +0800

    new file:   3-1 	new file:   3-1.cpp

---
+++ 3-1.cpp
@@ -0,0 +1,14 @@
+//1.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a << "+" << b << "=" << a + b << endl;
+}
+/*
+输入： 3 5
+输出：_________
+参考答案：3+5=8
+*/
